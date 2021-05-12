50419c3275adaf5e71c97823d15f4e11e25464f7
madhuridv <madhuridv3@gmail.com>
2021-05-11 18:01:56 -0700

    Bug Fix

--- frontend/src/components/InfoBar/index.js
+++ frontend/src/components/InfoBar/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-//import onlineIcon from "../../images/onlineIcon.png";
-//import closeIcon from "../../images/closeIcon.png";
+import onlineIcon from "../../images/onlineIcon.png";
+import closeIcon from "../../images/closeIcon.png";
 
 import "./InfoBar.css";
 
 const InfoBar = ({ room }) => (
   <div className="infoBar">
     <div className="leftInnerContainer">
-       {/* <img className="onlineIcon" src={onlineIcon} alt="online icon" />  */}
+      <img className="onlineIcon" src={onlineIcon} alt="online icon" />  
       <h3>{room}</h3>
     </div>
     <div className="rightInnerContainer">
       <a href="/chatlist">
-    {/* <img src={closeIcon} alt="close icon" />  */}
+     <img src={closeIcon} alt="close icon" />  
       </a>
     </div>
   </div>
