fdce38f4b8975c82078d9549da9e45eda3d022a8
Monil Sakhidas <monilsnehal.sakhidas@sjsu.edu>
2021-05-12 19:23:20 -0700

    Final Changes 2

--- backend/rentalAgreement/routes.js
+++ backend/rentalAgreement/routes.js
@@ -43,106 +43,106 @@ router.post("/create", requireSignIn, async (req, res) => {
   };
 
   // Design tenant information input schema
-  const tenantSchema = Joi.object({
-    name: Joi.string().required().messages({
-      "string.empty": "Enter a valid property type",
-      "any.required": "Enter a valid property type",
-    }),
-    parentName: Joi.string().required().messages({
-      "string.empty": "Enter a valid property type",
-      "any.required": "Enter a valid property type",
-    }),
-    address: Joi.string().required().messages({
-      "string.empty": "Enter a valid property type",
-      "any.required": "Enter a valid property type",
-    }),
-    email: Joi.string().email().required().messages({
-      "string.email": "Enter a valid email.",
-      "any.required": "Enter a valid email",
-    }),
-    panNumber: Joi.string(),
-  });
+  // const tenantSchema = Joi.object({
+  //   name: Joi.string().required().messages({
+  //     "string.empty": "Enter a valid property type",
+  //     "any.required": "Enter a valid property type",
+  //   }),
+  //   parentName: Joi.string().required().messages({
+  //     "string.empty": "Enter a valid property type",
+  //     "any.required": "Enter a valid property type",
+  //   }),
+  //   address: Joi.string().required().messages({
+  //     "string.empty": "Enter a valid property type",
+  //     "any.required": "Enter a valid property type",
+  //   }),
+  //   email: Joi.string().email().required().messages({
+  //     "string.email": "Enter a valid email.",
+  //     "any.required": "Enter a valid email",
+  //   }),
+  //   panNumber: Joi.string(),
+  // });
 
-  Design input schema
-  const schema = Joi.object({
-    clientType: Joi.string()
-      .valid(
-        config.RENT_AGREEMENT_LANDLORD_CLIENT,
-        config.RENT_AGREEMENT_TENANT_CLIENT
-      )
-      //      .required()
-      .messages({
-        "any.required": "Select a valid client type",
-      }),
-    propertyType: Joi.string().required().messages({
-      "string.empty": "Enter a valid property type",
-      "any.required": "Enter a valid property type",
-    }),
-    propertyState: Joi.string().required().messages({
-      "string.empty": "Enter a valid state",
-      "any.required": "Enter a valid state",
-    }),
-    propertyCity: Joi.string().required().messages({
-      "string.empty": "Enter a valid city",
-      "any.required": "Enter a valid city",
-    }),
-    propertyFloor: Joi.number().integer().min(0).max(200).messages({
-      "number.integer": "Enter a valid floor",
-      "number.min": "Enter a valid floor",
-      "number.max": "Enter a valid floor",
-      "number.base": "Enter a valid floor",
-      "any.required": "Enter a valid floor",
-    }),
-    propertyAddress: Joi.string().min(10).required().messages({
-      "string.empty": "Enter a valid address",
-      "string.min": "Enter a valid address",
-      "any.required": "Enter a valid address",
-    }),
-    propertyNumber: Joi.string().min(1).required().messages({
-      "string.empty": "Enter a valid house number",
-      "string.min": "Enter a valid house number",
-      "any.required": "Enter a valid house number",
-    }),
-    propertyLocality: Joi.string(),
-    propertyZipCode: Joi.string().min(6).max(6).required().messages({
-      "string.empty": "Enter a valid zipcode",
-      "string.min": "Enter a valid zipcode",
-      "string.max": "Enter a valid zipcode",
-      "any.required": "Enter a valid zipcode",
-    }),
-    agreementStartDate: Joi.date().required().messages({
-      "date.base": "Enter a valid agreement date",
-      "any.required": "Enter a valid date",
-    }),
-    rentPaymentDate: Joi.number().integer().min(1).max(31).messages({
-      "number.integer": "Enter a rent payment date",
-      "number.min": "Enter a rent payment date",
-      "number.max": "Enter a rent payment date",
-      "number.base": "Enter a rent payment date",
-      "any.required": "Enter a rent payment date",
-    }),
-    monthlyRent: Joi.number().positive().required().messages({
-      "number.positive": "Enter a valid rent amount",
-      "any.required": "Enter a valid rent amount",
-    }),
-    noticePeriod: Joi.number().positive().required().messages({
-      "number.positive": "Enter a valid notice period duration",
-      "any.required": "Enter a valid notice period duration",
-    }),
-    securityAmount: Joi.number().positive().required().messages({
-      "number.positive": "Enter a valid security deposit amount",
-      "any.required": "Enter a valid security deposit amount",
-    }),
-    tenants: Joi.array().items(tenantSchema).required(),
-    personalTerms: Joi.array().items(Joi.string()),
-  });
+  //Design input schema
+  // const schema = Joi.object({
+  //   clientType: Joi.string()
+  //     .valid(
+  //       config.RENT_AGREEMENT_LANDLORD_CLIENT,
+  //       config.RENT_AGREEMENT_TENANT_CLIENT
+  //     )
+  //     //      .required()
+  //     .messages({
+  //       "any.required": "Select a valid client type",
+  //     }),
+  //   propertyType: Joi.string().required().messages({
+  //     "string.empty": "Enter a valid property type",
+  //     "any.required": "Enter a valid property type",
+  //   }),
+  //   propertyState: Joi.string().required().messages({
+  //     "string.empty": "Enter a valid state",
+  //     "any.required": "Enter a valid state",
+  //   }),
+  //   propertyCity: Joi.string().required().messages({
+  //     "string.empty": "Enter a valid city",
+  //     "any.required": "Enter a valid city",
+  //   }),
+  //   propertyFloor: Joi.number().integer().min(0).max(200).messages({
+  //     "number.integer": "Enter a valid floor",
+  //     "number.min": "Enter a valid floor",
+  //     "number.max": "Enter a valid floor",
+  //     "number.base": "Enter a valid floor",
+  //     "any.required": "Enter a valid floor",
+  //   }),
+  //   propertyAddress: Joi.string().min(10).required().messages({
+  //     "string.empty": "Enter a valid address",
+  //     "string.min": "Enter a valid address",
+  //     "any.required": "Enter a valid address",
+  //   }),
+  //   propertyNumber: Joi.string().min(1).required().messages({
+  //     "string.empty": "Enter a valid house number",
+  //     "string.min": "Enter a valid house number",
+  //     "any.required": "Enter a valid house number",
+  //   }),
+  //   propertyLocality: Joi.string(),
+  //   propertyZipCode: Joi.string().min(6).max(6).required().messages({
+  //     "string.empty": "Enter a valid zipcode",
+  //     "string.min": "Enter a valid zipcode",
+  //     "string.max": "Enter a valid zipcode",
+  //     "any.required": "Enter a valid zipcode",
+  //   }),
+  //   agreementStartDate: Joi.date().required().messages({
+  //     "date.base": "Enter a valid agreement date",
+  //     "any.required": "Enter a valid date",
+  //   }),
+  //   rentPaymentDate: Joi.number().integer().min(1).max(31).messages({
+  //     "number.integer": "Enter a rent payment date",
+  //     "number.min": "Enter a rent payment date",
+  //     "number.max": "Enter a rent payment date",
+  //     "number.base": "Enter a rent payment date",
+  //     "any.required": "Enter a rent payment date",
+  //   }),
+  //   monthlyRent: Joi.number().positive().required().messages({
+  //     "number.positive": "Enter a valid rent amount",
+  //     "any.required": "Enter a valid rent amount",
+  //   }),
+  //   noticePeriod: Joi.number().positive().required().messages({
+  //     "number.positive": "Enter a valid notice period duration",
+  //     "any.required": "Enter a valid notice period duration",
+  //   }),
+  //   securityAmount: Joi.number().positive().required().messages({
+  //     "number.positive": "Enter a valid security deposit amount",
+  //     "any.required": "Enter a valid security deposit amount",
+  //   }),
+  //   tenants: Joi.array().items(tenantSchema).required(),
+  //   personalTerms: Joi.array().items(Joi.string()),
+  // });
 
   // Validate input
-  const result = await schema.validate(rawRentalagreementData);
-  if (result.error) {
-    res.status(400).send({ errorMessage: result.error.details[0].message });
-    return;
-  }
+  // const result = await schema.validate(rawRentalagreementData);
+  // if (result.error) {
+  //   res.status(400).send({ errorMessage: result.error.details[0].message });
+  //   return;
+  // }
   // find a lawyer
   const lawyerArray = await findLawyerWithMinimumActiveCases();
   const lawyer = lawyerArray[0];
